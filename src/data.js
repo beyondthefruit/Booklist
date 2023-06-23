e9a1c2e20b2af1318a2cae5eed437874fa539b1d
Kevin Hanard <kevin.hanard@gmail.com>
2023-06-23 10:07:51 +1000

    data book

--- src/data.js
+++ src/data.js
@@ -9,7 +9,7 @@ const bookData = [
     desc: 'On the world called Hyperion, beyond the law of the Hegemony of Man, there waits the creature called the Shrike. There are those who worship it. There are those who fear it. And there are those who have vowed to destroy it. In the Valley of the Time Tombs, where huge, brooding structures move backward through time, the Shrike waits for them all. On the eve of Armageddon, with the entire galaxy at war, seven pilgrims set forth on a final voyage to Hyperion seeking the answers to the unsolved riddles of their lives. Each carries a desperate hope—and a terrible secret. And one may hold the fate of humanity in his hands.',
     rating: 10,
     review:
-      'Seven pilgrims, each with a story to tell, travelling to Hyperion in the midst of an intergalactic conflict. Who is the Shrike? The traitor? Some characters are more interesting than others, I personnaly love the unlikeable poet Martin Silenius, but also the consul. What science fiction should look like',
+      'Seven pilgrims, each with a story to tell, travelling to Hyperion in the midst of an intergalactic conflict. Who is the Shrike? The traitor? Unique and interesting characters, I, personaly love the unlikeable poet Martin Silenius, and the consul. What science fiction should look like',
   },
   {
     id: 2,
@@ -20,7 +20,7 @@ const bookData = [
     desc: "Haviland Tuf is an honest space-trader who likes cats. So how is it that, in competition with the worst villains the universe has to offer, he's become the proud owner of the last seedship of Earth's legendary Ecological Engineering Corps? Never mind, just be thankful that the most powerful weapon in human space is in good hands-hands which now control cellular material for thousands of outlandish creatures. With his unique equipment, Tuf is set to tackle the problems human settlers have created in colonizing far-flung worlds: hosts of hostile monsters, a population hooked on procreation, a dictator who unleashes plagues to get his own way...and in every case the only thing that stands between the colonists and disaster is Tuf's ingenuity - and his reputation as an honest dealer in a universe of rogues...",
     rating: 10,
     review:
-      'My first non-TSOIAF from George R.R Martin, this book is brilliant, full of humour, well in advance on its time. Haviland is one of the most interesting character that I had the chance to readHopefully George will spare some time to publish more adventure of Haviland Tuf',
+      'My first non-TSOIAF from George R.R Martin, this book is brilliant, full of humour, well in advance of its time. Haviland is one of the most interesting characters that I had the chance to read. Hopefully, George will spare some time to publish more adventures of Haviland Tuf',
   },
   {
     id: 3,
@@ -33,7 +33,7 @@ const bookData = [
     desc: "The protagonist, Piscine Molitor 'Pi' Patel, a Tamil boy from Pondicherry, explores issues of spirituality and practicality from an early age. He survives 227 days after a shipwreck while stranded on a boat in the Pacific Ocean with a Bengal tiger named Richard Parker.",
     rating: 7,
     review:
-      "Interesting survival story, at time slightly boredom but still a decent book. I wouldn't want to be left stranded with a tiger on a boat",
+      "Interesting survival story, at times slightly boringbut still a decent book. I wouldn't want to be left stranded with a tiger on a boat",
   },
   {
     id: 4,
@@ -46,7 +46,7 @@ const bookData = [
     desc: "To Winston Smith, a young man who works in the Ministry of Truth (Minitru for short), come two people who transform this life completely. One is Julia, whom he meets after she hands him a slip reading, 'I love you.' The other is O'Brien, who tells him, 'We shall meet in the place where there is no darkness.' The way in which Winston is betrayed by the one and, against his own desires and instincts, ultimately betrays the other, makes a story of mounting drama and suspense.",
     rating: 7,
     review:
-      "A classic, you heard about it, it's on your reading list but you might think that it's too old to appreciated. So yes, the technologies are different but now more than ever this book is relevant. Clear your browser's cookies use a VPN, Big brother is watching you :)",
+      "A classic, everyone heard about it, it's probably on your reading list but you might think that it's too old. So yes, technology is different but now more than ever this book is relevant. Clear your browser's cookies, use a VPN, Big Brother is watching you :)",
   },
   {
     id: 5,
@@ -59,7 +59,7 @@ const bookData = [
     desc: "Winter is coming. Such is the stern motto of House Stark, the northernmost of the fiefdoms that owe allegiance to King Robert Baratheon in far-off King's Landing. There Eddard Stark of Winterfell rules in Robert's name. There his family dwells in peace and comfort: his proud wife, Catelyn; his sons Robb, Brandon, and Rickon; his daughters Sansa and Arya; and his bastard son, Jon Snow. Far to the north, behind the towering Wall, lie savage Wildings and worse--unnatural things relegated to myth during the centuries-long summer, but proving all too real and all too deadly in the turning of the season. Yet a more immediate threat lurks to the south, where Jon Arryn, the Hand of the King, has died under mysterious circumstances. Now Robert is riding north to Winterfell, bringing his queen, the lovely but cold Cersei, his son, the cruel, vainglorious Prince Joffrey, and the queen's brothers Jaime and Tyrion of the powerful and wealthy House Lannister--the first a swordsman without equal, the second a dwarf whose stunted stature belies a brilliant mind. All are heading for Winterfell and a fateful encounter that will change the course of kingdoms. Meanwhile, across the Narrow Sea, Prince Viserys, heir of the fallen House Targaryen, which once ruled all of Westeros, schemes to reclaim the throne with an army of barbarian Dothraki--whose loyalty he will purchase in the only coin left to him: his beautiful yet innocent sister, Daenerys.",
     rating: 10,
     review:
-      "A masterpiece, G.R.R Martin is brilliant, the characters are well developed and once you start reading it you can't stop. Even if you have seen Game of Thrones you have to read it as there is so much content in the book. Unfortunately, you come at a point where you have to stop, two more books have to be published to concluded one of the best fantasy saga",
+      "A masterpiece, G.R.R Martin is brilliant, the characters are well developed and once you start reading it you can't stop. Even if you have seen Game of Thrones you have to read it as there is so much content in the book. Unfortunately, you come to a point where you have to stop, two more books have to be published to conclude one of the best fantasy saga",
   },
   {
     id: 6,
@@ -69,10 +69,9 @@ const bookData = [
     photo:
       'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1541174620i/77565.jpg',
 
-    desc: 'On the world of Hyperion, the mysterious Time Tombs are opening. And the secrets they contain mean that nothing--nothing anywhere in the universe--will ever be the same',
+    desc: 'On the world of Hyperion, the mysterious Time Tombs are opening. And the secrets they contain mean that nothing--nothing anywhere in the universe--will ever be the same. In the second book in the Hyperion Cantos, your favourite characters are at the end of their journey and ready to fulfil their destiny and meet the Shrike.',
     rating: 10,
-    review:
-      'Second book in the Hyperion Cantos, your favourite character are at the end of their journey and ready to fulfill their destiny and meet the Shrike',
+    review: 'If you liked the first book, this is a must-to read',
   },
   {
     id: 7,
@@ -84,7 +83,7 @@ const bookData = [
     desc: "Gathered here in Volume I are the very best of George R.R. Martin's early works, including never-before-published fan pieces, his Hugo, Nebula, and Bram Stoker Award-winning stories plus the original novella The Ice Dragon, from which Martin's New York Times bestselling children's book of the same title originated. A dazzling array that features extensive author commentary, Dreamsongs, Volume I, is the perfect collection for both Martin devotees and a new generation of fans.",
     rating: 9,
     review:
-      'Part bibiographical, full of George R.R Martin comments on his life, his works. Perfect introduction to horror stories and some Award-winning works such as: A song for Lya, Nightflyers, ...',
+      "Part bibliographical, full of George R.R Martin's comments on his life, and his works. The perfect introduction to horror stories and some Award-winning works. Among my favourite stories:  A Song for Lya, the Monkey Treatment, Pear shaped Man, Nightflyers and many more.",
   },
   {
     id: 8,
@@ -95,7 +94,7 @@ const bookData = [
       'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/7528/9780752890098.jpg',
     desc: 'Whether writing about werewolves, wizards, or outer space, George R.R. Martin is renowned for his versatility and expansive talent, highlighted in this dazzling collection. Included here, in Volume II, are acclaimed stories such as the World Fantasy Award-winner The Skin Trade; as well as the first novella in the Ice and Fire universe, The Hedge Knight;, plus two never-before-published screenplays. Featuring extensive author commentary, Dreamsongs, Volume II, is an invaluable chronicle of a writer at the height of his creativity — and an unforgettable reading experience for fans old and new.',
     rating: 9,
-    review: 'Brilliant as always',
+    review: 'As brilliant as the first volume',
   },
 
   {
@@ -108,7 +107,7 @@ const bookData = [
     desc: "The lush and unique photography in this book represents National Geographic's Photo Ark, a major initiative and lifelong project by photographer Joel Sartore to make portraits of the world's animals-especially those that are endangered. His powerful message, conveyed with humor, compassion, and art: to know these animals is to save them. Sartore is circling the globe, visiting zoos and wildlife rescue centers to create studio portraits of 12,000 species, with an emphasis on those facing extinction. With a goal of photographing every animal in captivity in the world, he has photographed more than 6,000 already and now, thanks to a multi-year partnership with National Geographic, he may reach his goal. This book showcases his animal portraits: from tiny to mammoth, from the Florida grasshopper sparrow to the greater one-horned rhinoceros.",
     rating: 10,
     review:
-      'Creating a photo Ark of animals. A great project for a great cause',
+      'A project of the utmost importance, creating a photo Ark of every living animal on earth. I like the fact that animals are on a black or white background, Buying this book directly supports this great project.',
   },
   {
     id: 10,
@@ -120,7 +119,7 @@ const bookData = [
     desc: 'Come on a journey through global history, told in 130 maps that each offer a window on a key event. Step into the action and follow Ghengis Khan sweeping through China, Napoleon conquering Europe, or two world wars raging across the globe. See empires rise and fall - from the Egyptians and the Aztecs to the British empire and the Soviet Union.',
     rating: 10,
     review:
-      "I've always been interested in Historym this book cover many of our history most important event through maps",
+      "I've always been interested in history and curious of knowing more, this book covers the most important events through maps.",
   },
   {
     id: 11,
@@ -132,7 +131,7 @@ const bookData = [
     desc: " The Girl with the Dragon Tattoo Harriet Vanger, a scion of one of Sweden’s wealthiest families disappeared without a trace more than forty years ago. All these years later, her aged uncle continues to try to discover what happened to her. He hires Mikael Blomkvist, a journalist recently sidelined by a libel conviction, to investigate. Blomkvist is aided by the pierced and tattooed computer prodigy Lisbeth Salander. Together they tap into a vein of unfathomable iniquity and astonishing corruption on their way to discovering the truth of Harriet Vanger’s fate. The Girl Who Played with Fire Mikael Blomkvist, now the crusading publisher of the magazine Millennium, has decided to run a story that will expose an extensive sex trafficking operation. On the eve of its publication, the two reporters responsible for the article are murdered, and the fingerprints found on the murder weapon belong to his friend Lisbeth Salander. Blomkvist, convinced of Salander’s innocence, plunges into an investigation of the murders. Meanwhile, Salander herself is drawn into a murderous game of cat and mouse, which forces her to face her dark past. The Girl Who Kicked the Hornet's Nest Lisbeth Salander lies in critical condition, a bullet wound to her head, in the intensive care unit of a Swedish city hospital. She’s fighting for her life in more ways than one: if and when she recovers, she’ll be taken back to Stockholm to stand trial for three murders. With the help of Mikael Blomkvist, she will not only have to prove her innocence, but also identify and denounce those in authority who have allowed the vulnerable, like herself, to suffer abuse and violence. On her own, she will plot revenge—against the man who tried to kill her, and against the corrupt government institutions that very nearly destroyed her life.",
     rating: 9,
     review:
-      'One of the first book that I read in english, a mix between political and thriller',
+      'This political thriller is particularly important to me as it is the first book that I read in English. Stieg Larsson wrote a masterpiece.',
   },
   {
     id: 12,
@@ -144,7 +143,7 @@ const bookData = [
     desc: 'After his grandfather dies, avid scholar and budding forensic investigator Cí Song begrudgingly gives up his studies to help his family. But when another tragedy strikes, he’s forced to run and also deemed a fugitive. Dishonored, he has no choice but to accept work as a lowly gravedigger, a position that allows him to sharpen his corpse-reading skills. Soon, he can deduce whether a person killed himself—or was murdered.His prowess earns him notoriety, and Cí receives orders to unearth the perpetrator of a horrific series of mutilations and deaths at the Imperial Court. Cí’s gruesome investigation quickly grows complicated thanks to old loyalties and the presence of an alluring, enigmatic woman. But he remains driven by his passion for truth—especially once the killings threaten to take down the Emperor himself. Inspired by Song Cí, considered to be the founding father of CSI-style forensic science, this harrowing novel set during the thirteenth-century Tsong Dynasty draws readers into a multilayered, ingenious plot as disturbing as it is fascinating.',
     rating: 7,
     review:
-      "I love history but don't know much about the impressive Chinese culture, so was pleased by the book. On the down side, I quickly guessed who was responsible for Ci problems",
+      "I love history but don't know much about Chinese culture, so was pleased by the book. On the downside, I quickly guessed who was responsible for Ci's problems, but still a nice book.",
   },
   {
     id: 13,
@@ -155,7 +154,7 @@ const bookData = [
       'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/5750/9780575070707.jpg',
     desc: "Herbert's evocative, epic tales are set on the desert planet Arrakis, the focus for a complex political and military struggle with galaxy-wide repercussions. This volume includes the titles Dune, Dune Messiah and Children of Dune..",
     rating: 9,
-    review: '',
+    review: 'A classic in the field of science fiction.',
   },
   {
     id: 14,
@@ -166,7 +165,8 @@ const bookData = [
       'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1560930580i/43617306.jpg',
     desc: "Written between the years 1917 and 1935, this collection features Lovecraft's trademark fantastical creatures and supernatural thrills, as well as many horrific and cautionary science-fiction themes that have influenced some of today's writers and filmmakers, including Stephen King, Alan Moore, F. Paul Wilson, Guillermo del Toro, and Neil Gaiman.",
     rating: '',
-    review: 'currently reading',
+    review:
+      'Currently reading it following the advice of G.R.R Martin, some novels offer a frustrating ending to great stories, but some are incredibly good. My favorite: The colour out of space, the case of Charles Dexter Ward.',
   },
   {
     id: 15,
@@ -230,14 +230,26 @@ const bookData = [
   {
     id: 20,
     title: 'I am Pilgrim',
-    author: 'Terry',
+    author: 'Terry Hayes',
     genre: ['Fiction'],
     photo:
       'https://www.booktopia.com.au/covers/big/9780552160964/0000/i-am-pilgrim.jpg',
     desc: "The astonishing story of one man's breakneck race against time to save America from oblivion. A FATHER PUBLICLY BEHEADED. Killed in the blistering heat of a Saudi Arabian public square. A YOUNG WOMAN DISCOVERED. All of her identifying characteristics dissolved by acid. A SYRIAN BIOTECH EXPERT FOUND EYELESS. Dumped in a Damascus junkyard. SMOULDERING HUMAN REMAINS. Abandoned on a remote mountainside in Afghanistan.",
     rating: 9,
     review:
-      'My first spy/thriller, while focusing mainly on the life of the Pilgrim, the author succeed in giving a human side to the worst terrorist, beautifully written',
+      'My first spy/thriller, while focusing mainly on the life of the Pilgrim, the author succeed in giving a human side to the worst terrorist ever. I liked how the investigations were connected, beautifully written',
+  },
+  {
+    id: 21,
+    title: 'Animal Farm',
+    author: 'George Orwell',
+    genre: ['Political Satire'],
+    photo:
+      'https://www.dymocks.com.au/Pages/ImageHandler.ashx?q=9780141036137&w=&h=570',
+    desc: 'Mr Jones of Manor Farm is so lazy and drunken that one day he forgets to feed his livestock. The ensuing rebellion under the leadership of the pigs Napoleon and Wellington leads to the animals taking over the farm. Vowing to eliminate the terrible inequities of the farmyard, the renamed Animal Farm is organized to benefit all who walk on four legs. But as time passes, the ideals of the rebellion are corrupted, then forgotten. And something new and unexpected emerges ...',
+    rating: 9,
+    review:
+      'This is a really good book, easy to read and easily understood on how communist countries/ The leader, keep explaining how harvest are getting bigger and how life is far more pleasant than when their were living under the tyranny of humans. / History keep repeating itself.',
   },
   // {
   //   id: 17,
